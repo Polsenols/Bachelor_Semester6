c1e06d27ca1259748e995f17197a3a083c8d1438
Mads\MadsKD <mads_did@hotmail.com>
2017-04-23 15:45:25 +0200

    Fixed movement indicator

--- BachelorProject/Assets/Scripts/MoveManipulator.cs
+++ BachelorProject/Assets/Scripts/MoveManipulator.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveManipulator : MonoBehaviour {
+public class MoveManipulator : MonoBehaviour
+{
 
-    public float moveMultiplier = 0.05f;
+    public float moveDampener = 10f;
     public LayerMask indicatorLayer;
 
     private GameObject x_axis;
@@ -15,26 +16,26 @@ public class MoveManipulator : MonoBehaviour {
 
     private GameObject selectedAxis;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
         x_axis = gameObject.transform.Find("x-axis").gameObject;
         y_axis = gameObject.transform.Find("y-axis").gameObject;
         z_axis = gameObject.transform.Find("z-axis").gameObject;
         rootObject = gameObject.transform.root.gameObject;
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            if(Physics.Raycast(ray, out hit, 1000f, indicatorLayer.value))
+            if (Physics.Raycast(ray, out hit, 1000f, indicatorLayer.value))
             {
                 print("axis hit");
-                if(hit.collider.gameObject == x_axis)
+                if (hit.collider.gameObject == x_axis)
                 {
                     print("X-axis hit");
                     StartCoroutine(MoveX_axis());
@@ -53,32 +54,37 @@ public class MoveManipulator : MonoBehaviour {
             }
 
         }
-	}
+    }
 
     private IEnumerator MoveX_axis()
     {
         print("coroutine started");
         Vector3 mouseCurrentPos = Input.mousePosition;
+        float startXpos = rootObject.transform.position.x;
         while (Input.GetMouseButton(0))
         {
-            Vector3 diff = Input.mousePosition - mouseCurrentPos;
-            mouseCurrentPos = Input.mousePosition;
+            Vector3 diff = (Input.mousePosition - mouseCurrentPos) / moveDampener;
+            float dotp = Vector3.Dot(Camera.main.transform.right, diff.normalized);
+            Debug.Log(dotp);
 
-            rootObject.transform.position += rootObject.transform.right * (diff.x * moveMultiplier);
+            float difference = Mathf.Abs(diff.x) * Mathf.Sign(dotp) + startXpos;
+            rootObject.transform.position = new Vector3(difference, rootObject.transform.position.y, rootObject.transform.position.z);
             yield return null;
-        }        
+        }
     }
 
     private IEnumerator MoveY_axis()
     {
         print("coroutine started");
         Vector3 mouseCurrentPos = Input.mousePosition;
+        float startYpos = rootObject.transform.position.y;
         while (Input.GetMouseButton(0))
         {
-            Vector3 diff = Input.mousePosition - mouseCurrentPos;
-            mouseCurrentPos = Input.mousePosition;
-
-            rootObject.transform.position += rootObject.transform.up * (diff.y * moveMultiplier);
+            Vector3 diff = (Input.mousePosition - mouseCurrentPos) / moveDampener;
+            float dotp = Vector3.Dot(Camera.main.transform.up, diff.normalized);
+            Debug.Log(dotp);
+            float difference = Mathf.Abs(diff.y) * Mathf.Sign(dotp) + startYpos;
+            rootObject.transform.position = new Vector3(rootObject.transform.position.x, difference, rootObject.transform.position.z);
             yield return null;
         }
     }
@@ -87,12 +93,14 @@ public class MoveManipulator : MonoBehaviour {
     {
         print("coroutine started");
         Vector3 mouseCurrentPos = Input.mousePosition;
+        float startZpos = rootObject.transform.position.z;
         while (Input.GetMouseButton(0))
         {
-            Vector3 diff = Input.mousePosition - mouseCurrentPos;
-            mouseCurrentPos = Input.mousePosition;
-
-            rootObject.transform.position += rootObject.transform.forward * (diff.x * moveMultiplier);
+            Vector3 diff = (Input.mousePosition - mouseCurrentPos) / moveDampener;
+            float dotp = Vector3.Dot(Camera.main.transform.forward, diff.normalized);
+            Debug.Log(dotp);
+            float difference = Mathf.Abs(diff.z) * Mathf.Sign(dotp) + startZpos;
+            rootObject.transform.position = new Vector3(rootObject.transform.position.x, rootObject.transform.position.y, difference);
             yield return null;
         }
     }
--- BachelorProject/Assets/Scripts/ScaleManipulator.cs
+++ BachelorProject/Assets/Scripts/ScaleManipulator.cs
@@ -5,15 +5,76 @@ public class ScaleManipulator : MonoBehaviour
 {
     public Transform box_forward, box_up, box_right;
     public Transform line_forward, line_up, line_right;
+    public LayerMask mask;
+    public float moveMultiplier;
+    private Transform currentBoxSelected;
 
     void Start()
     {
 
     }
 
+    private enum MoveType
+    {
+        Forward,
+        Up,
+        Right
+    }
+
     void Update()
     {
         SetLineScale();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 1000f, mask))
+            {
+                if(hit.transform == box_forward)
+                {
+                    currentBoxSelected = box_forward;
+                }
+                else if (hit.transform == box_up)
+                {
+                    currentBoxSelected = box_up;
+                }
+                else if (hit.transform == box_right)
+                {
+                    currentBoxSelected = box_right;
+                }
+            }
+        }
+
+    }
+
+    void MoveBoxManipulator()
+    {
+
+    }
+
+    private IEnumerator Move_axis(Vector3 direction, MoveType moveType)
+    {
+        print("coroutine started");
+        Vector3 mouseCurrentPos = Input.mousePosition;
+        while (Input.GetMouseButton(0))
+        {
+            Vector3 diff = Input.mousePosition - mouseCurrentPos;
+            mouseCurrentPos = Input.mousePosition;
+            switch (moveType)
+            {
+                case MoveType.Forward:
+                    currentBoxSelected.position += direction * (diff.y * moveMultiplier);
+                    break;
+                case MoveType.Up:
+                    currentBoxSelected.position += direction * (diff.y * moveMultiplier);
+                    break;
+                case MoveType.Right:
+                    currentBoxSelected.position += direction * (diff.y * moveMultiplier);
+                    break;
+            }
+            yield return null;
+        }
     }
 
     void SetLineScale()
