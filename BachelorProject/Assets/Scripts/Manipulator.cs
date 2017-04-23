92ab46bd27a96ea9d78fd23502026f53c1823c94
Mads\MadsKD <mads_did@hotmail.com>
2017-04-23 18:59:13 +0200

    Fixed Movement tool

---
+++ BachelorProject/Assets/Scripts/Manipulator.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manipulator : MonoBehaviour
+{
+
+    public Transform target;
+    public LayerMask selectMask, ManipMask;
+    public ManipulatorType currentManip;
+    public float moveMultiplier, rotationMultiplier, scaleMultiplier;
+    public KeyCode SetMove = KeyCode.W;
+    public KeyCode SetRotate = KeyCode.E;
+    public KeyCode SetScale = KeyCode.R;
+    public GameObject moveObj, rotateObj, scaleObj;
+
+    private Camera cam;
+    Vector3 originalTargetPosition;
+
+    public enum ManipulatorType
+    {
+        Move,
+        Rotate,
+        Scale
+    }
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 1000f, ManipMask))
+            {
+                Debug.Log("hit manip");
+                StartCoroutine(TransformObject(currentManip, hit.transform.gameObject));
+            }
+            else if (Physics.Raycast(ray, out hit, 1000f, selectMask))
+            {
+                target = hit.transform;
+                SetManipulatorPos();
+                originalTargetPosition = target.position;
+            }
+        }
+        SetManipType();
+    }
+
+    void SetManipulatorPos()
+    {
+        scaleObj.transform.SetParent(target);
+        rotateObj.transform.SetParent(target);
+        moveObj.transform.SetParent(target);
+        moveObj.transform.localPosition = Vector3.zero;
+        moveObj.transform.localRotation = Quaternion.identity;
+
+    }
+
+    void SetManipType()
+    {
+        if (Input.GetKeyDown(SetMove))
+        {
+            currentManip = ManipulatorType.Move;
+            scaleObj.SetActive(false);
+            rotateObj.SetActive(false);
+            moveObj.SetActive(true);
+        }
+        else if (Input.GetKeyDown(SetRotate))
+        {
+            scaleObj.SetActive(false);
+            rotateObj.SetActive(true);
+            moveObj.SetActive(false);
+            currentManip = ManipulatorType.Rotate;
+        }
+        else if (Input.GetKeyDown(SetScale))
+        {
+            scaleObj.SetActive(true);
+            rotateObj.SetActive(false);
+            moveObj.SetActive(false);
+            currentManip = ManipulatorType.Scale;
+        }
+    }
+
+    Vector3 GetSelectedAxis(GameObject axis)
+    {
+        if (axis.CompareTag("X"))
+        {
+            return target.right;
+        }
+        else if (axis.CompareTag("Y"))
+        {
+            return target.up;
+        }
+        else if (axis.CompareTag("Z"))
+        {
+            return target.forward;
+        }
+        return Vector3.one;
+    }
+
+    IEnumerator TransformObject(ManipulatorType type, GameObject axisObj)
+    {
+        if (target == null)
+        {
+            Debug.Log("Target is null, breaking");
+            yield break;
+        }
+        Vector3 planeNormal = (transform.position - target.position).normalized;
+        Vector3 axis = GetSelectedAxis(axisObj);
+        Vector3 projectedAxis = Vector3.ProjectOnPlane(axis, planeNormal).normalized;
+        Vector3 previousMousePosition = Vector3.zero;
+        while (Input.GetMouseButton(0))
+        {
+            Ray mouseRay = cam.ScreenPointToRay(Input.mousePosition);
+            Vector3 mousePos = Geometry.LinePlaneIntersect(mouseRay.origin, mouseRay.direction, originalTargetPosition, planeNormal);
+
+            if (previousMousePosition != Vector3.zero && mousePos != Vector3.zero)
+            {
+                switch (currentManip)
+                {
+                    case ManipulatorType.Move:
+                        float movementAmount = Vector3.Dot((mousePos - previousMousePosition), projectedAxis) * moveMultiplier;
+                        target.Translate(axis * movementAmount, Space.World);
+                        break;
+                }
+            }
+
+            previousMousePosition = mousePos;
+            yield return null;
+        }
+    }
+}
--- BachelorProject/Assets/Scripts/MoveManipulator.cs
+++ BachelorProject/Assets/Scripts/MoveManipulator.cs
@@ -7,6 +7,7 @@ public class MoveManipulator : MonoBehaviour
 
     public float moveDampener = 10f;
     public LayerMask indicatorLayer;
+    public Material selectedMat;
 
     private GameObject x_axis;
     private GameObject y_axis;
@@ -16,6 +17,9 @@ public class MoveManipulator : MonoBehaviour
 
     private GameObject selectedAxis;
 
+    public Renderer x_axisRender, y_axisRender, z_axisRender;
+    private Material original_X_Mat, original_Z_Mat, original_Y_Mat;
+
     // Use this for initialization
     void Start()
     {
@@ -23,6 +27,9 @@ public class MoveManipulator : MonoBehaviour
         y_axis = gameObject.transform.Find("y-axis").gameObject;
         z_axis = gameObject.transform.Find("z-axis").gameObject;
         rootObject = gameObject.transform.root.gameObject;
+        original_X_Mat = x_axisRender.material;
+        original_Z_Mat = z_axisRender.material;
+        original_Y_Mat = y_axisRender.material;
     }
 
     // Update is called once per frame
@@ -59,6 +66,7 @@ public class MoveManipulator : MonoBehaviour
     private IEnumerator MoveX_axis()
     {
         print("coroutine started");
+        x_axis.GetComponent<Renderer>().material = selectedMat;
         Vector3 mouseCurrentPos = Input.mousePosition;
         float startXpos = rootObject.transform.position.x;
         while (Input.GetMouseButton(0))
@@ -71,11 +79,13 @@ public class MoveManipulator : MonoBehaviour
             rootObject.transform.position = new Vector3(difference, rootObject.transform.position.y, rootObject.transform.position.z);
             yield return null;
         }
+        x_axis.GetComponent<Renderer>().material = original_X_Mat;
     }
 
     private IEnumerator MoveY_axis()
     {
         print("coroutine started");
+        y_axis.GetComponent<Renderer>().material = selectedMat;
         Vector3 mouseCurrentPos = Input.mousePosition;
         float startYpos = rootObject.transform.position.y;
         while (Input.GetMouseButton(0))
@@ -87,21 +97,32 @@ public class MoveManipulator : MonoBehaviour
             rootObject.transform.position = new Vector3(rootObject.transform.position.x, difference, rootObject.transform.position.z);
             yield return null;
         }
+        y_axis.GetComponent<Renderer>().material = original_Y_Mat;
     }
 
     private IEnumerator MoveZ_axis()
     {
         print("coroutine started");
+        z_axis.GetComponent<Renderer>().material = selectedMat;
         Vector3 mouseCurrentPos = Input.mousePosition;
+        //Vector3 planeNormal = (transform.position - target.position).normalized;
         float startZpos = rootObject.transform.position.z;
         while (Input.GetMouseButton(0))
         {
+            
             Vector3 diff = (Input.mousePosition - mouseCurrentPos) / moveDampener;
             float dotp = Vector3.Dot(Camera.main.transform.forward, diff.normalized);
             Debug.Log(dotp);
-            float difference = Mathf.Abs(diff.z) * Mathf.Sign(dotp) + startZpos;
+            float difference = Mathf.Abs(diff.x*diff.y) * Mathf.Sign(dotp) + startZpos;
             rootObject.transform.position = new Vector3(rootObject.transform.position.x, rootObject.transform.position.y, difference);
             yield return null;
         }
+        z_axis.GetComponent<Renderer>().material = original_Z_Mat;
+    }
+
+    public static float MagnitudeInDirection(Vector3 vector, Vector3 direction, bool normalizeParameters = true)
+    {
+        if (normalizeParameters) direction.Normalize();
+        return Vector3.Dot(vector, direction);
     }
 }
